7409d26181479adb9cb43cf07723185e7c4ae317
FARIEL523 <masdar5769@gmail.com>
2025-04-13 23:54:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,10 @@ function tampilkan(halaman) {
   <audio controls>
     <source src="Musik.mp3" type="audio/mpeg">
   </audio>
+
+   <video width="320" height="240" controls>
+    <source src="Video.mp4" type="video/mp4">
+  </video>
 `;
 
   }
