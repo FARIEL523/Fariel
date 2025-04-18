c11e83efec14b128522b7688a89618a9cdc86c1d
Fariel <masdar5769@gmail.com>
2025-04-18 20:41:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -30,48 +30,47 @@ function tampilkan(halaman) {
   </ul>
   <p style="text-align: center; font-weight: bold;">10 TEKS</p>
   
-  <img src="Calibri" alt="10 Teks" width="300"><br><br>
-  <img src="TimesNewRoman" alt="10 Teks" width="300"><br><br>
-  <img src="Arial" alt="10 Teks" width="300"><br><br>
-  <img src="Helvetica" alt="10 Teks" width="300"><br><br>
-  <img src="Garamond" alt="10 Teks" width="300"><br><br>
-  <img src="CourierNew" alt="10 Teks" width="300"><br><br>
-  <img src="ComicSans" alt="10 Teks" width="300"><br><br>
-  <img src="Verdana" alt="10 Teks" width="300"><br><br>
-  <img src="Georgia" alt="10 Teks" width="300"><br><br>
-  <img src="Roboto" alt="10 Teks" width="300"><br><br>
+  <img src="Calibri" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
+  <img src="TimesNewRoman" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
+  <img src="Arial" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
+  <img src="Helvetica" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
+  <img src="Garamond" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
+  <img src="CourierNew" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
+  <img src="ComicSans" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
+  <img src="Verdana" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
+  <img src="Georgia" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
+  <img src="Roboto" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
   <p style="text-align: center; font-weight: bold;">10 GAMBAR</p>
-  
-  <p>1. JPEG</p>
-  <img src="FotoMeme.jpg" alt="10 Gambar" width="300"><br><br>
+  <p style="text-align: center;">1. JPEG</p>
+  <img src="FotoMeme.jpg" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
-  <p>2. PNG</p>
-  <img src="FotoMeme.png" alt="10 Gambar" width="300"><br><br>
+  <p style="text-align: center;">2. PNG</p>
+  <img src="FotoMeme.png" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
-  <p>3. GIF</p>
-  <img src="FotoMeme.gif" alt="10 Gambar" width="300"><br><br>
+  <p style="text-align: center;">3. GIF</p>
+  <img src="FotoMeme.gif" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
-  <p>4. BMP</p>
-  <img src="FotoMeme.bmp" alt="10 Gambar" width="300"><br><br>
+  <p style="text-align: center;">4. BMP</p>
+  <img src="FotoMeme.bmp" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
-  <p>5. SVG</p>
-  <img src="FotoMeme.svg" alt="10 Gambar" width="300"><br><br>
+  <p style="text-align: center;">5. SVG</p>
+  <img src="FotoMeme.svg" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
-  <p>6. TIFF</p>
-  <img src="FotoMeme.tiff" alt="10 Gambar" width="300"><br><br>
+  <p style="text-align: center;">6. TIFF</p>
+  <img src="FotoMeme.tiff" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
-  <p>7. WEBP</p>
-  <img src="FotoMeme.webp" alt="10 Gambar" width="300"><br><br>
+  <p style="text-align: center;">7. WEBP</p>
+  <img src="FotoMeme.webp" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
-  <p>8. HEIC</p>
-  <img src="FotoMeme.heic" alt="10 Gambar" width="300"><br><br>
+  <p style="text-align: center;">8. HEIC</p>
+  <img src="FotoMeme.heic" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
-  <p>9. RAW</p>
-  <img src="FotoMeme_1744979634.raw" alt="10 Gambar" width="300"><br><br>
+  <p style="text-align: center;">9. RAW</p>
+  <img src="FotoMeme_1744979634.raw" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
-  <p>10. pdf</p>
-  <img src="FotoMeme.pdf" alt="10 Gambar" width="300"><br><br>
+  <p style="text-align: center;">10. PDF</p>
+  <img src="FotoMeme.pdf" alt="10 Teks" width="300" style="display: block; margin: auto;"><br><br>
 
 
 `;
